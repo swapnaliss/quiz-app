9618372902ed1e815c797a1fde7346739a138bdf
swapnaliss <swapnalisbhargude123@gmail.com>
2022-09-13 11:15:16 +0530

    feat - added array of objects

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 var readlineSync = require("readline-sync");
+
+var questions = [
+  {
+    question: "Who is invented Javascript ?",
+    answer: "Brendan Eich",
+  },
+  {
+    question: "when it is invented ?",
+    answer: "1995",
+  },
+  {
+    question: "where was javascript invented ?",
+    answer: "Netscape Communications Corporation",
+  },
+  {
+    question: "What is the first name of JavaScript ?",
+    answer: "Mocha",
+  },
+  {
+    question: "Which came first Java or JavaScript?",
+    answer: "Java",
+  },
+];
